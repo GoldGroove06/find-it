4c1cce051082e6967d10fcb0be0ac645090f9f63
Arshpreet Singh <73437174+GoldGroove06@users.noreply.github.com.>
2025-03-31 01:35:14 +0530

    populate db

---
+++ db/populatedb.js
@@ -0,0 +1,27 @@
+const { Client } = require("pg");
+
+const SQL = `
+CREATE TABLE IF NOT EXISTS usernames (
+  id INTEGER PRIMARY KEY NOT NULL,
+  name VARCHAR ( 255 ),
+  time INTEGER
+);
+
+`;
+
+async function main() {
+  console.log("seeding...");
+  const client = new Client({
+    host: process.env.HOST, 
+    user: process.env.USER,
+    database: process.env.DATABASE,
+    password: process.env.PASS,
+    port: process.env.POSTGRES_PORT 
+  });
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+  console.log("done");
+}
+
+main();
